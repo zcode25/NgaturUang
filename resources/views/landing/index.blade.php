199cff2f24b327caccce329c3b0bb825918c6d56
zcode25 <adamzein345@gmail.com>
2025-07-26 12:21:08 +0700

    favicon landing

--- resources/views/landing/index.blade.php
+++ resources/views/landing/index.blade.php
@@ -3,7 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>NgaturUang - Kelola Keuangan Pribadi</title>
+    <link rel="shortcut icon" href="/assets/images/favicon.png" type="image/x-icon" />
+    <title>NgaturUang</title>
 
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
